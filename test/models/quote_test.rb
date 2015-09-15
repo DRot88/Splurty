76eb52e18130948f3b8cf8f02d9640a4fb7f7997
Daniel <Daniel.Rotenberg88@gmail.com>
2015-09-15 00:09:56 +0000

    Built test for unique tag, and updated view

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  
+  test "unique_tag" do
+    quote = Quote.create(:author => 'Daniel Rotenberg', :saying => 'You only yolo once.')
+    expected = "DR#" + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
+
 end
