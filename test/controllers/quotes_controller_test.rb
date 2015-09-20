e2d5fbdd633b33d4c4c4094b505fa921f9299cc6
Daniel Rotenberg <Daniel.Rotenberg88@gmail.com>
2015-09-20 01:31:32 -0400

    Edited text on main quote page

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -12,4 +12,14 @@ class QuotesControllerTest < ActionController::TestCase
     get :show, :id => 'omg'
     assert_response :not_found
   end
+
+  test "create a quote success" do
+
+  end
+
+  test "create a quote validation error" do
+
+  end
+
+
 end
